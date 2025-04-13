ab160ed3f1fa17a24585ed2ebcb2d0fb127ee1d1
chirashnudas <77160750+chirashnudas@users.noreply.github.com>
2025-04-13 22:35:23 +0530

    Update Footer.jsx

--- 3d-portfolio-main/src/sections/Footer.jsx
+++ 3d-portfolio-main/src/sections/Footer.jsx
@@ -7,13 +7,21 @@ const Footer = () => {
         <div className="flex flex-col justify-center">
           <p>Terms & Conditions</p>
         </div>
+
         <div className="socials">
           {socialImgs.map((socialImg, index) => (
             <div key={index} className="icon">
-              <img src={socialImg.imgPath} alt="social icon" />
+              <a
+                href={socialImg.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={socialImg.imgPath} alt={`${socialImg.name} icon`} />
+              </a>
             </div>
           ))}
         </div>
+
         <div className="flex flex-col justify-center">
           <p className="text-center md:text-end">
             © {new Date().getFullYear()} Chirashnu Das. All rights reserved.
